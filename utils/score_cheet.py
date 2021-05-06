9de1702009dea8c711591dde0fe4593c4abde719
mouradap <drmouradap@gmail.com>
2021-05-06 17:10:16 -0300

    Adding CCI_Score class and calculations

---
+++ utils/score_cheet.py
@@ -0,0 +1,18 @@
+scores = {"age": [0, 1, 2, 3, 4],
+            "infarct": [0, 1],
+            "fcc": [0, 1],
+            "pvd": [0, 1],
+            "cva": [0 ,1],
+            "dementia": [0 ,1],
+            "copd": [0, 1],
+            "ctd": [0, 1],
+            "pud": [0, 1],
+            "liver": [0, 1, 3],
+            "diabetes": [0, 1, 2],
+            "hemiplegia": [0, 2],
+            "ckd": [0, 2],
+            "tumor": [0, 2, 6],
+            "leukemia": [0, 2],
+            "lymphoma": [0, 2],
+            "hiv": [0, 6]
+}
